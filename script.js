52b7ef0581f92ef2c48d65342c2787e3f41ae817
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2021-12-09 18:55:22 -0300

    Requisito 7 e 8 - ok

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let colorBlack = document.getElementsByClassName('color c1')[0];
+let colorRed = document.getElementsByClassName('color c2')[0];
+let colorBlue = document.getElementsByClassName('color c3')[0];
+let colorGreen = document.getElementsByClassName('color c4')[0];
+let board = document.getElementById('pixel-board');
+
 
 function appendPixel(){
     let pixelBoard = document.getElementById('pixel-board');
@@ -9,3 +15,49 @@ function appendPixel(){
 for(i = 0; i < 25; i+=1){
     appendPixel();        
 }
+
+function selectColor(color){
+    colorBlack.classList.remove('selected');
+    colorRed.classList.remove('selected');
+    colorBlue.classList.remove('selected');
+    colorGreen.classList.remove('selected');
+    color.classList.add('selected');
+}
+
+function painting(){
+    board.addEventListener('click', function (event){
+        if (colorBlack.classList[2] == "selected"){
+            event.target.style.background = "Black";
+        }else if (colorRed.classList[2] == "selected"){
+            event.target.style.background = "Red";
+        }else if (colorBlue.classList[2] == "selected"){
+            event.target.style.background = "Blue";
+        }else if (colorGreen.classList[2] == "selected"){
+            event.target.style.background = "Green";
+        }
+    });
+}
+
+
+window.onload = function () {
+
+    colorBlack.addEventListener('click', function(event){
+        selectColor(colorBlack);
+    });
+    colorRed.addEventListener('click', function (event){
+        selectColor(colorRed);
+    });
+    colorBlue.addEventListener('click', function (event){
+        selectColor(colorBlue);
+    });
+    colorGreen.addEventListener('click', function (event){
+        selectColor(colorGreen);
+    });
+    board.addEventListener('click', function (event){
+        event.target.style.background = "red";
+    });
+    painting();
+};
+
+//DIV                 .remove
+//newElement.classList.add('selected')
